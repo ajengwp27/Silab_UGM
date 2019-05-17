20578d66b84c34629be3776b1adf3e4508f5e1e1
AlbertusNdaru <albertusndarukrismandoko@gmail.com>
2019-05-17 15:13:34 +0700

    Update database.php
    Updae Online

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'db_peternakan_ugm',
+	'hostname' => 'sempalan.co.id',
+	'username' => 'sempalan_ndaru',
+	'password' => 'Jarumblack1',
+	'database' => 'sempalan_peternakan_ugm',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
