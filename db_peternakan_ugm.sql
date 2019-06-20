1821c47161e3412a5cdbdf2f5a0ef508c67f076e
ajeng <ajeng300@gmail.com>
2019-06-20 13:43:19 +0700

    Revert "Update db_peternakan_ugm.sql"
    This reverts commit df73ea6e82adf5995ff6e29b94722d0a0f3b46f7.

--- db_peternakan_ugm.sql
+++ db_peternakan_ugm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2019 at 08:32 AM
+-- Generation Time: Jun 20, 2019 at 05:04 AM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -30,12 +30,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tb_alat` (
   `id_alat` int(11) NOT NULL,
-  `Name` varchar(30) DEFAULT NULL,
-  `Categori_id` int(11) DEFAULT NULL,
-  `Number_of_rack` varchar(20) DEFAULT NULL,
-  `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Name` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
+  `Category_id` int(11) DEFAULT NULL,
+  `Number_of_rack` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,18 +45,10 @@ CREATE TABLE `tb_alat` (
 
 CREATE TABLE `tb_category` (
   `Category_id` int(10) NOT NULL,
-  `Name_Category` varchar(30) DEFAULT NULL,
+  `Name_Category` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
   `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `Update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_category`
---
-
-INSERT INTO `tb_category` (`Category_id`, `Name_Category`, `Create_at`, `Update_at`) VALUES
-(6, 'Test sadsfsfsdfsd sdfdsf', '2019-05-17 15:53:22', '2019-05-16 14:20:50'),
-(9, 'test kategori 4', '2019-05-17 15:58:20', '2019-05-17 22:52:56');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -83,7 +75,7 @@ CREATE TABLE `tb_hak_akses` (
   `id_level` int(11) NOT NULL,
   `Description` varchar(40) DEFAULT NULL,
   `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `Update_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -91,8 +83,8 @@ CREATE TABLE `tb_hak_akses` (
 --
 
 INSERT INTO `tb_hak_akses` (`id_level`, `Description`, `Create_at`, `Update_at`) VALUES
-(1, 'Superadmin', '2019-06-14 03:27:00', '2019-06-14 10:27:00'),
-(2, 'Admin', '2019-06-14 03:27:00', '2019-06-14 10:27:00');
+(1, 'Superadmin', '2019-06-20 03:00:55', '0000-00-00 00:00:00'),
+(2, 'Admin', '2019-06-20 03:00:59', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -105,7 +97,7 @@ CREATE TABLE `tb_karyawan` (
   `Name` varchar(30) DEFAULT NULL,
   `Gender` varchar(2) DEFAULT NULL,
   `Address` varchar(70) DEFAULT NULL,
-  `Status` varchar(20) DEFAULT NULL,
+  `Status` varchar(20) DEFAULT 'Off',
   `Phone` varchar(15) DEFAULT NULL,
   `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -117,9 +109,7 @@ CREATE TABLE `tb_karyawan` (
 --
 
 INSERT INTO `tb_karyawan` (`id_karyawan`, `Name`, `Gender`, `Address`, `Status`, `Phone`, `Create_at`, `update_at`, `email_karyawan`) VALUES
-(1, 'Ajeng', 'P', 'Berbah', 'Aktif', '0876543332', '2019-06-17 15:54:12', '2019-06-17 22:54:12', 'ajeng300@gmail.com'),
-(4, 'nina', 'P', 'magelang', 'Aktif', '087665544444', '2019-06-17 15:18:32', '2019-06-13 22:11:01', 'ninabobo@gmail.com'),
-(5, 'Fani', 'P', 'banguntapan', 'Aktif', '089674075180', '2019-06-17 15:07:57', '2019-06-17 22:07:35', 'fani@gmail.com');
+(1, 'Ajeng', 'Pe', 'Baran', 'Aktif', '081', '2019-06-20 03:03:15', '2019-06-20 10:03:05', 'Ajeng300@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -161,20 +151,11 @@ CREATE TABLE `tb_mahasiswa` (
   `Gender` varchar(2) DEFAULT NULL,
   `Address` varchar(40) DEFAULT NULL,
   `Phone` varchar(15) DEFAULT NULL,
-  `Status` varchar(10) DEFAULT NULL,
+  `Status` varchar(10) DEFAULT 'Off',
   `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `Update_at` datetime DEFAULT NULL,
   `Email_mahasiswa` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_mahasiswa`
---
-
-INSERT INTO `tb_mahasiswa` (`id_mahasiswa`, `Nim`, `Name`, `Gender`, `Address`, `Phone`, `Status`, `Create_at`, `Update_at`, `Email_mahasiswa`) VALUES
-(1, '3163111043', 'Ajeng Wuriprastiwi A', 'P', 'baran', '089608000446', 'Off', '2019-06-14 02:53:57', '2019-05-17 23:19:59', 'ajengpraswi@gmail.com'),
-(3, '3163111042', 'Ajeng WP', 'P', 'baran', '089608000446', 'Aktif', '2019-06-14 02:56:05', NULL, 'ajengpraswi@gmail.com'),
-(5, '0140126553', 'Wuri', 'P', 'baran berbah sleman', '08765555777', 'Aktif', '2019-06-19 06:57:16', NULL, 'ajeng300@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -197,22 +178,21 @@ CREATE TABLE `tb_peminjaman_alat` (
 
 CREATE TABLE `tb_user` (
   `id_user` int(11) NOT NULL,
-  `Username` varchar(20) DEFAULT NULL,
-  `Password` varchar(20) DEFAULT NULL,
+  `Username` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `Password` varchar(20) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
   `id_level` int(11) DEFAULT NULL,
   `id_karyawan` int(11) DEFAULT NULL,
-  `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Status` varchar(10) DEFAULT 'Not Aprove'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `Status` varchar(10) CHARACTER SET latin1 DEFAULT 'NotAprove'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tb_user`
 --
 
 INSERT INTO `tb_user` (`id_user`, `Username`, `Password`, `id_level`, `id_karyawan`, `Create_at`, `Update_at`, `Status`) VALUES
-(1, 'Ajeng', 'Ajeng123', 1, 1, '2019-06-17 16:40:14', '2019-06-17 23:40:14', 'Aprove'),
-(3, 'AjengW', 'ajeng123', NULL, NULL, '2019-06-17 16:31:13', '2019-06-17 23:31:13', 'Aprove');
+(1, 'Ajeng', 'Ajeng123', 1, 1, '2019-06-20 03:03:45', '2019-06-20 10:03:45', 'Aprove');
 
 -- --------------------------------------------------------
 
@@ -235,7 +215,7 @@ CREATE TABLE `tb_user_mahasiswa` (
 --
 ALTER TABLE `tb_alat`
   ADD PRIMARY KEY (`id_alat`),
-  ADD KEY `Categori_id` (`Categori_id`);
+  ADD KEY `Categori_id` (`Category_id`);
 
 --
 -- Indexes for table `tb_category`
@@ -320,7 +300,7 @@ ALTER TABLE `tb_alat`
 -- AUTO_INCREMENT for table `tb_category`
 --
 ALTER TABLE `tb_category`
-  MODIFY `Category_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `Category_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_detail_peminjaman`
@@ -338,7 +318,7 @@ ALTER TABLE `tb_hak_akses`
 -- AUTO_INCREMENT for table `tb_karyawan`
 --
 ALTER TABLE `tb_karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_kegiatan`
@@ -356,7 +336,7 @@ ALTER TABLE `tb_kerusakan`
 -- AUTO_INCREMENT for table `tb_mahasiswa`
 --
 ALTER TABLE `tb_mahasiswa`
-  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tb_peminjaman_alat`
@@ -368,7 +348,7 @@ ALTER TABLE `tb_peminjaman_alat`
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_user_mahasiswa`
@@ -384,7 +364,7 @@ ALTER TABLE `tb_user_mahasiswa`
 -- Constraints for table `tb_alat`
 --
 ALTER TABLE `tb_alat`
-  ADD CONSTRAINT `tb_alat_ibfk_1` FOREIGN KEY (`Categori_id`) REFERENCES `tb_category` (`Category_id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `tb_alat_ibfk_1` FOREIGN KEY (`Category_id`) REFERENCES `tb_category` (`Category_id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tb_detail_peminjaman`
@@ -410,8 +390,8 @@ ALTER TABLE `tb_peminjaman_alat`
 -- Constraints for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  ADD CONSTRAINT `fk_id_karyawan` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_karyawan` (`id_karyawan`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `tb_hak_akses` (`id_level`);
+  ADD CONSTRAINT `fk_id_karyawan` FOREIGN KEY (`id_karyawan`) REFERENCES `tb_karyawan` (`id_karyawan`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_user_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `tb_hak_akses` (`id_level`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tb_user_mahasiswa`
